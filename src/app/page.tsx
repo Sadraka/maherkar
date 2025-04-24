25a57f13cfdc5a9b890dc42349f9fbcc320ffff1
Sadra <sadra.kabiri@gmail.com>
2025-04-24 11:35:39 +0330

    بروزرسانی صفحه اصلی با توضیحات فارسی و تطبیق با استراتژی جدید سایت

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+/**
+ * صفحه اصلی سایت ماهرکار
+ * پلتفرم کاریابی ساده و کارآمد که امکان انتشار آگهی‌های شغلی و ارتباط کارفرمایان و کارجویان را فراهم می‌کند
+ */
+
 import Header from '@/components/layout/Header';
 import Hero from '@/components/home/Hero';
 import JobListings from '@/components/home/JobListings';
@@ -10,13 +15,28 @@ import Footer from '@/components/layout/Footer';
 export default function Home() {
   return (
     <main>
+      {/* هدر سایت */}
       <Header />
+      
+      {/* بخش جستجوی صفحه اصلی */}
       <Hero />
+      
+      {/* بخش نمایش آگهی‌های شغلی */}
       <JobListings />
+      
+      {/* بخش نمایش متخصصین */}
       <Experts />
+      
+      {/* بخش استخدام روزانه */}
       <DailyHiring />
+      
+      {/* سوالات متداول */}
       <FAQ />
+      
+      {/* درباره ما */}
       <AboutUs />
+      
+      {/* فوتر سایت */}
       <Footer />
     </main>
   );
